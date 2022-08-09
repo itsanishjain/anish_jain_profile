d5a48368fba2dd3ba57da91efa14c289f1350f9b
anishjain <helloanishjain@gmail.com>
2022-08-09 23:58:14 +0530

    add new projects,remove unsused code and fix ui

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,162 @@
-import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import WorkCard from "../src/components/WorkCard";
-import BlackWorkCard from "../src/components/BlackWorkCard";
+
+import { works } from "../src/utils/myWorks";
+import logo from "../public/logo.png";
 
 export default function Home() {
   return (
-    // <div className="bg-gradient-to-b from-green-100">
-    //   <div className="pt-8 flex flex-col justify-center max-w-md mx-auto">
-    //     <div className="text-center">
-    //       <span className="text-2xl"> Hi, I&apos;m Anish - Blockend Developer</span>
-    //     </div>
-    //     <div className="flex justify-center space-x-4 mt-4" >
-    //       <span><a href="https://twitter.com/itsanishjain" target="_blank" rel="noreferrer" >Twitter</a></span>
+    <div className="bg-[#242424] text-white text-lg">
+      <div className="flex justify-between items-center p-2">
+        <p className="text-2xl font-bold">
+          <Image src={logo} width={100} height={100} alt="logo" />
+        </p>
+      </div>
 
-    //       <span><a href="https://github.com/itsanishjain" target="_blank" rel="noreferrer">Github</a></span>
+      <div className="mt-8 flex flex-col justify-center items-center space-y-4">
+        <p className="text-md font-medium text-red-500">Namaste</p>
+        <p className="text-2xl">I&apos;m Anish Jain</p>
+        <p className="text-3xl">Full stack blockchain developer</p>
+        <div className="flex space-x-2">
+          <a
+            href="mailto: helloanishjain@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+            className="rounded-lg bg-red-700 p-4"
+          >
+            HIRE ME
+          </a>
+          <Link href="#WORK">
+            <a className="rounded-lg bg-red-700 p-4">MY WORKS</a>
+          </Link>
+        </div>
+        <div className="flex space-x-2">
+          <span>
+            <a
+              href="https://twitter.com/itsanishjain"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Twitter
+            </a>
+          </span>
 
-    //       <span><a href="https://www.youtube.com/channel/UC_k5xjI9j35RyDGQl-MEaQQ" target="_blank" rel="noreferrer">Youtube</a></span>
+          <span>
+            <a
+              href="https://github.com/itsanishjain"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+          </span>
 
-    //       <span><a href="/anish_resume_2.pdf" target="_blank" rel="noreferrer" >Resume</a></span>
-    //     </div>
-    //   </div>
-    //   <div className="mt-8 p-4">
-    //     <hr />
-    //     <WorkCard />
-    //   </div>
-    // </div>
+          <span>
+            <a
+              href="https://www.youtube.com/channel/UC_k5xjI9j35RyDGQl-MEaQQ"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Youtube
+            </a>
+          </span>
 
-    <BlackWorkCard />
+          <span>
+            <a href="/jain_resume.pdf" target="_blank" rel="noreferrer">
+              Resume
+            </a>
+          </span>
+        </div>
+      </div>
 
+      {/* ABOUT */}
+
+      <div className="flex flex-col justify-center items-center mt-8 space-y-4 max-w-lg mx-auto">
+        <p className="text-4xl text-red-500">About Me</p>
+        <p className="text-center">
+          I&apos;m a passionate software engineer specialized in building full
+          stack websites and web applications for individuals and small
+          businesses!
+        </p>
+      </div>
+
+      {/* The Works */}
+      <div id="WORK" className="flex flex-col space-y-4 mt-16 items-center">
+        <p className="text-4xl text-red-500">Works</p>
+        <p className="text-center text-lg">
+          It is important for all my clients to know how I can build you a
+          successful web site.
+        </p>
+
+        {/* Card */}
+
+        <div
+          className="max-w-3xl mx-auto grid 
+              grid-row-1 gap-8 p-2 md:grid-cols-2 md:gap-4"
+        >
+          {works.map((work, index) => (
+            <div
+              key={index}
+              className="border-2 border-white text-black 
+                      rounded-lg p-1"
+            >
+              <img src={work.img} alt="work" />
+              <div className="p-4 space-y-2 h-full w-full">
+                <p className="text-white">{work.title}</p>
+                <hr />
+                <div className="flex space-x-4">
+                  <a
+                    href={work.liveUrl}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="px-4 py-2 text-sm cursor-pointer duration-100  rounded-md bg-red-700 text-white"
+                  >
+                    Live
+                  </a>
+
+                  {work.githubUrl && (
+                    <a
+                      href={work.githubUrl}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="px-4 py-2 text-sm cursor-pointer duration-100  rounded-md bg-red-700 text-white"
+                    >
+                      Github
+                    </a>
+                  )}
+
+                  {work.videoUrl && (
+                    <a
+                      href={work.videoUrl}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="px-4 py-2 text-sm cursor-pointer duration-100  rounded-md bg-red-700 text-white"
+                    >
+                      Youtube
+                    </a>
+                  )}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* Skills */}
+      <div className="mt-16 flex flex-col items-center space-y-4">
+        <p className="text-4xl text-red-500">Skills</p>
+        <p>JavaScript</p>
+        <p>SOLIDITY</p>
+        <p>NEXTJS</p>
+        <p>Python</p>
+        <p>HTML</p>
+        <p>Making others happy 😂️</p>
+      </div>
+
+      {/* Footer */}
+      <div className="mt-16 text-center pb-6">
+        Copyright ©2022 All rights reserved
+      </div>
+    </div>
   );
 }
--- src/utils/myWorks.js
+++ src/utils/myWorks.js
@@ -1,81 +1,118 @@
 export const works = [
-    {
-        title: "A tool that allows creator to get whitelisting for their upcoming project. We are going to create a slew of tools to assist creators/artists in obtaining the finest possible allowlist for their projects.",
-        img: "/allowlist.png",
-        date: "Jan 2022",
-        liveUrl: "https://www.allowlist.live/",
-        githubUrl: "https://github.com/itsanishjain/allowlist",
-        videoUrl: "https://youtu.be/QssdpvEeokk"
-    },
-    {
-        title: "A tool that helps you to create an NFTs like a PRO  ",
-        img: "/minter.png",
-        date: "Jan 2022",
-        liveUrl: "https://minter-bay.vercel.app/",
-        githubUrl: "https://github.com/itsanishjain/minter"
+  {
+    title:
+      "3L3phants project is an NFT collection consisting of 7777 unique 3L3s.",
+    img: "/3l3.webp",
+    date: "May 2022",
+    liveUrl: "https://www.3l3phants.io/",
+  },
+  {
+    title:
+      "Kostbar Für immer is a collection of 6,100 utility-powered artifacts that give you membership access to Kostbarworld. Kostbar holders receive access to exclusive drops, experiences, and much more. Visit Kostbarworld.xyz for more details.",
+    img: "https://user-images.githubusercontent.com/43285598/181042743-dbc36e03-f32b-4fcc-aa3b-d5124a2bb320.png",
+    date: "Aug 2022",
+    liveUrl: "https://www.kostbarmint.xyz/",
+  },
+  {
+    title:
+      "Metaverse platform where you became a “LANDLORD” generate montly rentanls",
+    img: "/lol.jpg",
+    date: "Aug 2022",
+    liveUrl: "https://lordsofthelands.io/",
+  },
 
-    },
-    {
-        title: "Motive Is to learn more about Solidity and how to connect smart contracts with Frontend like NextJS. And to understand how much we store in the blockchain if you visit this website please try to post a job and see how much we store in the blockchain.",
-        img: "/web3jobs.png",
-        date: "Jan 2022",
-        liveUrl: "https://web3-job-board.vercel.app/",
-        githubUrl: "https://github.com/itsanishjain/web3-job-board"
-    },
-    {
-        title: "Game Build using Smart contractsuser can mint NFTs base on Unique Dna strore on the blockchian. Once user Own There NFTs they can play with by providing them Various game itemsThey can increate level of it",
-        img: "https://user-images.githubusercontent.com/43033153/168064200-94c93a81-a443-4d59-b9de-b25d57649767.png",
-        date: "Mar 2022",
-        liveUrl: "https://void-nerd.vercel.app/",
-        githubUrl: "https://github.com/itsanishjain/VoidNerd"
-    },
-    {
-        title: "Web3-Tribes is to allow users to join different web3 tribes (communities) and gain access to content only visible to those in that specific tribe.",
-        img: "https://user-images.githubusercontent.com/78753275/156931795-8520fced-e450-4272-990d-cd9f0162a522.png",
-        date: "Mar 2022",
-        liveUrl: "https://web3con-lw-3-tribe.vercel.app",
-        githubUrl: "https://github.com/itsanishjain/Web3con-LW3Tribe"
-    },
-    {
-        title: "it is a really simple tool that allows anyone to send their ICX in a very simple and intuitive way to their favorite content creators.",
-        img: "https://camo.githubusercontent.com/343c660f872fcc1a155914967cf70252f82b8e5f5660fa1b5343ff3a0b493d52/68747470733a2f2f692e6962622e636f2f546d575a63304a2f53637265656e73686f742d66726f6d2d323032322d30312d33302d32322d30352d35302e706e67",
-        date: "Jan 2022",
-        liveUrl: "https://buy-me-icon-crypto.vercel.app/",
-        githubUrl: "https://github.com/itsanishjain/buyMeIconCrypto",
-        videoUrl: "https://www.youtube.com/watch?v=zAZ6Y5zy0XE"
-    },
+  {
+    title:
+      "A maketplace for content creators, find the Best Influencers for your Next Campaign in Seconds!",
+    img: "/whizco.png",
+    date: "May 2022",
+    liveUrl: "https://whizco.co/",
+  },
 
-    {
-        title: "Collection of my HTML,CSS and JS projects Here I used to learn basics of those things and sharing it over twitter",
-        img: "https://user-images.githubusercontent.com/43033153/168064674-3c9aca30-d685-4569-8e7c-e50e4daaacca.png",
-        date: "Feb 2022",
-        liveUrl: "https://itsanishjain.github.io/HTML-CSS-JS-boilerplate/",
-        githubUrl: "https://github.com/itsanishjain/HTML-CSS-JS-boilerplate"
-    },
-    {
-        title: " how to connect wallet connect and Metamask using web3 react, nextJS, and make it live so anyone can use it.  wallet connect, Metamask, connect wallets Dapps ",
-        img: "https://user-images.githubusercontent.com/43033153/168066309-b96167c9-5815-49ff-b7c0-9bd47e1614fb.png",
-        date: "Apr 2022",
-        liveUrl: "https://web3-wallet-five.vercel.app/",
-        githubUrl: "https://github.com/itsanishjain/web3-wallet",
-        videoUrl: "https://www.youtube.com/watch?v=mZMze_g5AkM"
-    },
-    {
-        title: "Collection of my dapps such as whitelistingDapp, MintingDapp, and ICO",
-        img: "https://user-images.githubusercontent.com/43033153/168067660-dc6f1df7-9101-4659-a703-4f69e76a8f5a.png",
-        date: "Feb-Apr 2022",
-        liveUrl: "https://dapp-red.vercel.app/",
-        githubUrl: "https://github.com/itsanishjain/dapp",
-        videoUrl: "https://www.youtube.com/watch?v=bDujU3YMEzk&list=PLgP_sOGeL8eWfjp2kt8mbq1IppjV0RbXL&index=1"
-    },
-    {
-        title: "it is a game build using using Polygon, JS, Web3-React and NftPort",
-        img: "/web3Snake.png",
-        date: "Feb-Apr 2022",
-        liveUrl: "https://web3-snakes.vercel.app/",
-        githubUrl: "https://github.com/itsanishjain/web3-snakes",
+  {
+    title:
+      "A tool that allows creator to get whitelisting for their upcoming project. We are going to create a slew of tools to assist creators/artists in obtaining the finest possible allowlist for their projects.",
+    img: "/allowlist.png",
+    date: "Jan 2022",
+    liveUrl: "https://www.allowlist.live/",
+    githubUrl: "https://github.com/itsanishjain/allowlist",
+    videoUrl: "https://youtu.be/QssdpvEeokk",
+  },
+  {
+    title:
+      "Motive Is to learn more about Solidity and how to connect smart contracts with Frontend like NextJS. And to understand how much we store in the blockchain if you visit this website please try to post a job and see how much we store in the blockchain.",
+    img: "/jobBoard.png",
+    date: "Jan 2022",
+    liveUrl: "https://web3-job-board.vercel.app/",
+    githubUrl: "https://github.com/itsanishjain/web3-job-board",
+  },
+  {
+    title: "A tool that helps you to create an NFTs like a PRO  ",
+    img: "/proMint.png",
+    date: "Feb 2022",
+    liveUrl: "https://pro-mint.vercel.app/",
+    githubUrl: "https://github.com/itsanishjain/proMint",
+  },
+  {
+    title:
+      "Web3-Tribes is to allow users to join different web3 tribes (communities) and gain access to content only visible to those in that specific tribe.",
+    img: "https://user-images.githubusercontent.com/78753275/156931795-8520fced-e450-4272-990d-cd9f0162a522.png",
+    date: "Mar 2022",
+    liveUrl: "https://web3con-lw-3-tribe.vercel.app",
+    githubUrl: "https://github.com/itsanishjain/Web3con-LW3Tribe",
+  },
 
-    },
-    
+  {
+    title:
+      "Game Build using Smart contractsuser can mint NFTs base on Unique Dna strore on the blockchian. Once user Own There NFTs they can play with by providing them Various game itemsThey can increate level of it",
+    img: "https://user-images.githubusercontent.com/43033153/168064200-94c93a81-a443-4d59-b9de-b25d57649767.png",
+    date: "Mar 2022",
+    liveUrl: "https://void-nerd.vercel.app/",
+    githubUrl: "https://github.com/itsanishjain/VoidNerd",
+  },
 
+  {
+    title:
+      "it is a really simple tool that allows anyone to send their ICX in a very simple and intuitive way to their favorite content creators.",
+    img: "https://camo.githubusercontent.com/343c660f872fcc1a155914967cf70252f82b8e5f5660fa1b5343ff3a0b493d52/68747470733a2f2f692e6962622e636f2f546d575a63304a2f53637265656e73686f742d66726f6d2d323032322d30312d33302d32322d30352d35302e706e67",
+    date: "Jan 2022",
+    liveUrl: "https://buy-me-icon-crypto.vercel.app/",
+    githubUrl: "https://github.com/itsanishjain/buyMeIconCrypto",
+    videoUrl: "https://www.youtube.com/watch?v=zAZ6Y5zy0XE",
+  },
+  {
+    title:
+      "Collection of my HTML,CSS and JS projects Here I used to learn basics of those things and sharing it over twitter",
+    img: "https://user-images.githubusercontent.com/43033153/168064674-3c9aca30-d685-4569-8e7c-e50e4daaacca.png",
+    date: "Feb 2022",
+    liveUrl: "https://itsanishjain.github.io/HTML-CSS-JS-boilerplate/",
+    githubUrl: "https://github.com/itsanishjain/HTML-CSS-JS-boilerplate",
+  },
+
+  {
+    title: "it is a game build using using Polygon, JS, Web3-React and NftPort",
+    img: "/web3Snake.png",
+    date: "Feb-Apr 2022",
+    liveUrl: "https://web3-snakes.vercel.app/",
+    githubUrl: "https://github.com/itsanishjain/web3-snakes",
+  },
+  {
+    title:
+      "Collection of my dapps such as whitelistingDapp, MintingDapp, and ICO",
+    img: "https://user-images.githubusercontent.com/43033153/168067660-dc6f1df7-9101-4659-a703-4f69e76a8f5a.png",
+    date: "Feb-Apr 2022",
+    liveUrl: "https://dapp-red.vercel.app/",
+    githubUrl: "https://github.com/itsanishjain/dapp",
+    videoUrl:
+      "https://www.youtube.com/watch?v=bDujU3YMEzk&list=PLgP_sOGeL8eWfjp2kt8mbq1IppjV0RbXL&index=1",
+  },
+  {
+    title:
+      " how to connect wallet connect and Metamask using web3 react, nextJS, and make it live so anyone can use it.  wallet connect, Metamask, connect wallets Dapps ",
+    img: "https://user-images.githubusercontent.com/43033153/168066309-b96167c9-5815-49ff-b7c0-9bd47e1614fb.png",
+    date: "Apr 2022",
+    liveUrl: "https://web3-wallet-five.vercel.app/",
+    githubUrl: "https://github.com/itsanishjain/web3-wallet",
+    videoUrl: "https://www.youtube.com/watch?v=mZMze_g5AkM",
+  },
 ];
