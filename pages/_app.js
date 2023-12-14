5c1f5c7dbbc05ea0e31b598affeaa8810142e62b
anish <helloanishjain@gmail.com>
2023-12-14 13:44:56 +0530

    add simpleanalytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,22 @@
 import "../styles/globals.css";
 import { Roboto } from "next/font/google";
+import Script from "next/script";
 
 const poppins = Roboto({ subsets: ["latin"], weight: ["400"] });
 
 function MyApp({ Component, pageProps }) {
   return (
     <main className={poppins.className}>
+      <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
+      <noscript>
+        {/* eslint-disable @next/next/no-img-element */}
+        <img
+          src="https://queue.simpleanalyticscdn.com/noscript.gif"
+          alt=""
+          referrerPolicy="no-referrer-when-downgrade"
+        />
+      </noscript>
+
       <Component {...pageProps} />
     </main>
   );
